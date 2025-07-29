a3968a2faa0803b50658b7e487556d61803bcccb
Shreya Kathe <shreyakathe2904@gmail.com>
2025-07-30 00:58:01 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,13 @@ import dotenv from "dotenv";
 dotenv.config({
     path: "./.env"
 });
-
+const PORT = process.env.PORT || 3000;
 
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log(` ⚙️ Server is running on port http://localhost:${process.env.PORT}`);
-        });
+      app.listen(PORT, () => {
+    console.log(`⚙️ Server is running on port ${PORT}`);
+});
     })
     .catch((error) => {
         console.error(` ❌ fail to connect: ${error.message}`);
